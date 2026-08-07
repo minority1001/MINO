51c6a32251e32dba978ad53259b9059a77f9f695
minority1001 <162978644+minority1001@users.noreply.github.com>
2026-08-07 10:51:19 +0700

    Update mino.js

--- mino.js
+++ mino.js
@@ -12,13 +12,10 @@ process.setMaxListeners(0);
 require("events").EventEmitter.defaultMaxListeners = 0;
 process.on('uncaughtException', function (err) {});
 
-// Fungsi untuk mendapatkan timestamp
+// Fungsi timestamp hanya waktu (HH:MM:SS)
 function getTimestamp() {
     const now = new Date();
-    return now.getFullYear() + '-' +
-           String(now.getMonth() + 1).padStart(2, '0') + '-' +
-           String(now.getDate()).padStart(2, '0') + ' ' +
-           String(now.getHours()).padStart(2, '0') + ':' +
+    return String(now.getHours()).padStart(2, '0') + ':' +
            String(now.getMinutes()).padStart(2, '0') + ':' +
            String(now.getSeconds()).padStart(2, '0');
 }
